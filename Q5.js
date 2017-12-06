e4fa1f489685af5dc63f6dee22f63a2a64782a10
Brian Sutachan <bsutachan@techlaunch.io>
2017-12-06 18:54:25 -0500

    first commit

---
+++ Q5.js
@@ -0,0 +1,6 @@
+// Log to the console a Boolean expression that tests
+// whether two numbers are equal, and whose answer evaluates to false.
+
+x = 5
+y = 6
+console.log(x == y)
---
+++ Q6.js
@@ -0,0 +1,5 @@
+// Log to the console a Boolean expression that
+// tests whether two numbers are not equal, and whose answer evaluates to true.
+x = 8
+y = 9
+console.log(x !== y)
